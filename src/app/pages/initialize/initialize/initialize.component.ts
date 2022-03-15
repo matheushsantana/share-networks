073bbb7c545d69bc891577cc3c8a5c568d8a0def
matheushsantana <53281663+matheushsantana@users.noreply.github.com>
2022-03-14 20:18:21 -0400

    att

--- src/app/pages/initialize/initialize/initialize.component.ts
+++ src/app/pages/initialize/initialize/initialize.component.ts
@@ -7,17 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitializeComponent implements OnInit {
 
-  etapas: number = 0;
+  etapas: number = 2;
+  subEtapa: number = 0;
+  auxSubEtapas: boolean = false;
 
-  titleEtapa: string[] =  ['', '', 'Monte seu Site', 'Boas-Vindas']
+  titleEtapa: string[] = ['', '', 'Monte seu Site', 'Boas-Vindas']
+  titleSubEtapa: string[] = ['Boas-Vindas', 'Apresentação', 'Catálogo', 'Links', 'Redes Sociais']
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  avanca(){
+  avanca(value: number) {
+    this.auxSubEtapas = false;
+    if (value == 0) {
+      if (this.etapas == 0) {
+      } else {
+        this.etapas--
+      }
+    } else {
+      this.etapas++
+    }
+  }
+
+  avancaOpcoes(value: number) {
+    this.auxSubEtapas = true
     this.etapas++
+    this.subEtapa = value
   }
 
 }
