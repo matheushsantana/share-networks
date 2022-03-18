a36e0fc98f34a67c03d01fc50f2e59a25cc81894
matheus.henrique <matheus.henrique@agriti.com.br>
2022-03-18 13:51:16 -0400

    att

--- src/app/pages/initialize/initialize/initialize.component.ts
+++ src/app/pages/initialize/initialize/initialize.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AdicionarLink } from '../shared/class/adicionarLink';
 import { Apresentacao } from '../shared/class/apresentacao';
 import { BoasVindas } from '../shared/class/boasVindas';
